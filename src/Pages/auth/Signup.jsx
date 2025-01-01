a762ef655bfb6c3192d147cb1edcddf01b2b8af0
Donald2023-source <donalddyusuf@gmail.com>
2025-01-01 15:50:33 -0800

    sign up

--- src/Pages/auth/Signup.jsx
+++ src/Pages/auth/Signup.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Signup = () => {
+
+    const [data, setData] = useState([])
+
+
+    const fetchStates = async() => {
+        try {
+            const response = await axios.get('https://nga-states-lga.onrender.com/fetch')
+            console.log(response)
+        } catch(error) {
+            console.error(error.message)
+        }   
+    }
+
+    useEffect(() => {
+        fetchStates()
+    }, [])
+
   return (
     <div className='w-full h-full flex'>
         <div className='w-1/3 bg-primary h-screen flex flex-col justify-center p-20 text-white'>
@@ -10,7 +28,7 @@ const Signup = () => {
 
         <div>
             <form>
-                
+                <h2>Get Started</h2>
             </form>
         </div>
     </div>
