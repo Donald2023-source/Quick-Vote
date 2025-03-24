32016e78af66b2e7de7d1b56d7c038d5963689ce
Donald2023-source <donalddyusuf@gmail.com>
2025-03-24 14:00:33 +0100

    authentication

--- src/Pages/auth/Signup.jsx
+++ src/Pages/auth/Signup.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import img from "../../assets/pexels-expect-best-79873-323776.jpg";
 const Signup = () => {
   return (
     <div className="px-10 h-screen py-3">
       <div className="border flex gap-10 h-full">
-        <div className="w-1/3 border">Image Section</div>
-        <div>Main Page</div>
+        <div className="w-1/3 border">
+          <img className="w-full h-full object-cover" src={img} alt="" />
+        </div>
+        <div>
+          <form action="">
+            <fieldset className="border px-4 py-1 rounded ">
+              <legend className="text-sm text-gray-400">Full Name</legend>
+              <input type="text" placeholder="Full Name" />
+            </fieldset>
+          </form>
+        </div>
       </div>
     </div>
   );
